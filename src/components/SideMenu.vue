96f925c38d97edb762825b17a80d0c9ac3d76c0d
Kevin Jay <kjayh@unal.edu.co>
2023-04-15 16:52:31 -0500

    add tailwind library and create home page

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-red-400 h-full flex flex-col w-[30%] justify-around rounded-e-[40px] min-w-[320px]">
+    <div class="flex justify-center">
+      <img src="http://via.placeholder.com/640x360" class="rounded-full w-[150px] h-[150px] mt-2" />
+    </div>
+    <div class="flex items-center justify-center flex-col">
+      <MenuButton text="Registrar/Editar mesas" route="register" />
+      <MenuButton text="Listado de mesas" route="about" />
+      <MenuButton text="Crear/Cancelar reserva" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MenuButton from './MenuButton.vue';
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import SideMenu from '@/components/SideMenu.vue';
+import TableButton from '@/components/TableButton.vue';
+import { ref } from 'vue';
+
+const tables = ref([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="h-screen flex">
+    <div>
+      <SideMenu />
+    </div>
+    <div class="flex justify-center items-center w-full">
+      <div class="grid grid-cols-3 gap-32">
+        <TableButton v-for="table in tables" :key="table" :number="table" />
+      </div>
+    </div>
+  </div>
 </template>
