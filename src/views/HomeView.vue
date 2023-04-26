079c352eb593d8ff62650f8a66b2d0eed793d2d2
kevinjay4567 <71843864+kevinjay4567@users.noreply.github.com>
2023-04-26 14:19:45 -0500

    Revert "Crud mesas"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import SideMenu from '@/components/SideMenu.vue';
-import TableButton from '@/components/TableButton.vue';
-import { ref } from 'vue';
-
-const tables = ref([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="h-screen flex">
-    <div>
-      <SideMenu />
-    </div>
-    <div class="flex justify-center items-center w-full">
-      <div class="grid grid-cols-3 gap-28">
-        <TableButton v-for="table in tables" :key="table" :number="table" />
-      </div>
-    </div>
-  </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
